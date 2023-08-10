2b44b2a397b7c09a21dafba35a6176bbe2ee9a8c
Bruno Stangarlin <bruno214214@gmail.com>
2023-08-10 20:29:05 -0300

    Seeding Service

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using MVC_WebApplication.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MVC_WebApplicationContext>
 (
@@ -9,24 +10,25 @@ builder.Services.AddDbContext<MVC_WebApplicationContext>
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<SeedingService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllerRoute
 (
     name: "default",
