4ff600f300bd666d4ef1b1684d05f07fc1c9a109
Bruno Stangarlin <bruno214214@gmail.com>
2023-08-10 20:56:59 -0300

    SellersController

---
+++ Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_WebApplication.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
